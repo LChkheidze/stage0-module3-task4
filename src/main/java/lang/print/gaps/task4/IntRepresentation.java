52477c5a5231d2a527c161a503682260a8327adf
LChkheidze <levanchx@gmail.com>
2023-05-08 22:24:51 +0400

    Types set, formula written

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,6 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        System.out.printf("%c, %c, %c, %c\n", 99, 104, 97, 114);
     }
 }
